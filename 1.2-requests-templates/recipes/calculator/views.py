a01ae7bc828ae4b2adb08a558761342e3ee198e0
Egor Makushkin <makushkin99@mail.com>
2024-03-26 23:22:50 +0300

    1.2-requests-templates updated

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -16,15 +16,23 @@ DATA = {
         'сыр, ломтик': 1,
         'помидор, ломтик': 1,
     },
-    # можете добавить свои рецепты ;)
 }
 
-# Напишите ваш обработчик. Используйте DATA как источник данных
-# Результат - render(request, 'calculator/index.html', context)
-# В качестве контекста должен быть передан словарь с рецептом:
-# context = {
-#   'recipe': {
-#     'ингредиент1': количество1,
-#     'ингредиент2': количество2,
-#   }
-# }
+
+def recipe(request, dish):
+    try:
+        servings = int(request.GET.get('servings', 1))
+        if servings != 1:
+            context = {
+                'recipe': {key: value * servings for key, value in DATA.get(dish).items()}
+            }
+        else:
+            context = {
+                'recipe': DATA.get(dish)
+            }
+        return render(request, 'calculator/index.html', context)
+    except AttributeError:
+        context = {
+            'recipe': None
+        }
+        return render(request, 'calculator/index.html', context)
