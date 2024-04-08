16bc0cac41048e5b3c8bc154aabaf9f4f2f97e80
Egor Makushkin <makushkin99@mail.com>
2024-04-08 21:17:34 +0300

    products (3.1) done

--- 3.1/products/main/serializers.py
+++ 3.1/products/main/serializers.py
@@ -1,16 +1,22 @@
 from rest_framework import serializers
 
+from main.models import Review, Product
+
 
 class ReviewSerializer(serializers.ModelSerializer):
-    # реализуйте все поля
-    pass
+    class Meta:
+        model = Review
+        fields = '__all__'
 
 
 class ProductListSerializer(serializers.Serializer):
-    # реализуйте поля title и price
-    pass
+    title = serializers.CharField(max_length=100)
+    price = serializers.DecimalField(max_digits=10, decimal_places=2)
 
 
 class ProductDetailsSerializer(serializers.ModelSerializer):
-    # реализуйте поля title, description, price и reviews (список отзывов к товару)
-    pass
+    comments = ReviewSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Product
+        fields = ['title', 'description', 'price', 'comments']
--- 3.1/products/main/views.py
+++ 3.1/products/main/views.py
@@ -1,32 +1,39 @@
+from django.http import Http404
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from main.models import Product, Review
 from main.serializers import ReviewSerializer, ProductListSerializer, ProductDetailsSerializer
 
 
 @api_view(['GET'])
 def products_list_view(request):
-    """реализуйте получение всех товаров из БД
-    реализуйте сериализацию полученных данных
-    отдайте отсериализованные данные в Response"""
-    pass
+    products = Product.objects.all()
+    ser = ProductListSerializer(products, many=True)
+    return Response(ser.data)
 
 
 class ProductDetailsView(APIView):
     def get(self, request, product_id):
-        """реализуйте получение товара по id, если его нет, то выдайте 404
-        реализуйте сериализацию полученных данных
-        отдайте отсериализованные данные в Response"""
-        pass
+        product_review = Product.objects.filter(id=product_id)
+        if not product_review:
+            raise Http404
+        else:
+            ser = ProductDetailsSerializer(product_review, many=True)
+            return Response(ser.data)
 
 
 # доп задание:
 class ProductFilteredReviews(APIView):
     def get(self, request, product_id):
-        """обработайте значение параметра mark и
-        реализуйте получение отзывов по конкретному товару с определённой оценкой
-        реализуйте сериализацию полученных данных
-        отдайте отсериализованные данные в Response"""
-        pass
+        mark = request.query_params.get('mark')
+        if not mark:
+            review = Review.objects.filter(product_id=product_id)
+        else:
+            review = Review.objects.filter(mark=mark, product_id=product_id)
+        ser = ReviewSerializer(review, many=True)
+        if not ser.data:
+            raise Http404
+        return Response(ser.data)
